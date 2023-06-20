73674df7cf6ba7e595d9995df503ca56832ac07d
brotherick14 <54830246+brotherick14@users.noreply.github.com>
2023-06-20 04:01:42 -0600

    feat: additional items (#4)

--- services/echoService.js
+++ services/echoService.js
@@ -4,17 +4,35 @@ const { processFile } = require('./helperService')
 const echoService = {
   filesData: async (req, res) => {
     try {
-      const response = await axiosInstance.get('/secret/files')
-      const fileDataPromises = response.data.files.map(processFile)
-      const formattedData = (await Promise.all(fileDataPromises)).filter(Boolean)
-
-      res.setHeader('Content-Type', 'application/json')
-      res.status(200).json(formattedData)
+      const response = await axiosInstance.get('/secret/files');
+      const { fileName } = req.query;
+  
+      // Filtrar archivos si se proporciona un nombre de archivo específico
+      const filteredFiles = fileName
+        ? response.data.files.filter((file) => file === fileName)
+        : response.data.files;
+  
+      const fileDataPromises = filteredFiles.map(processFile);
+      const formattedData = (await Promise.all(fileDataPromises)).filter(Boolean);
+  
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(formattedData);
     } catch (error) {
-      console.error('Error downloading files')
-      res.status(500).json({ error: 'An error occurred' })
+      console.error('Error downloading files');
+      res.status(500).json({ error: 'An error occurred' });
     }
-  }
+  },
+  listFiles: async (req, res) => {
+    try {
+      const response = await axiosInstance.get('/secret/files');
+  
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error('Error retrieving file names');
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  }  
 }
 
 module.exports = echoService
